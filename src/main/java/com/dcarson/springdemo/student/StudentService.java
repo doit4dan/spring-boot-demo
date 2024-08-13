47ab81b2ed29701f7b2ecd43e755872b304c7c7b
dcarson <danielcarson1234@gmail.com>
2024-08-13 00:19:44 -0400

    implementing dao layer for in memory data access

--- src/main/java/com/dcarson/springdemo/student/StudentService.java
+++ src/main/java/com/dcarson/springdemo/student/StudentService.java
@@ -1,28 +1,11 @@
 package com.dcarson.springdemo.student;
 
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
-    public List<Student> findAllStudents() {
-        return List.of(
-                new Student(
-                        "Dan",
-                        "Carson",
-                        LocalDate.now(),
-                        "contact@dcarson.com",
-                        25
-                ),
-                new Student(
-                        "Ana",
-                        "Carson",
-                        LocalDate.now(),
-                        "contact@acarson.com",
-                        25
-                )
-        );
-    }
+public interface StudentService {
+    Student save(Student s);
+    List<Student> findAllStudents();
+    Student findByEmail(String email);
+    Student update(Student s);
+    void delete(String email);
 }
